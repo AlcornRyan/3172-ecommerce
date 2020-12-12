3eea601d4eb4ad71707a01194645bfce40d3902d
Ryan Alcorn <ryanalcorn2@gmail.com>
2020-12-12 17:48:29 -0400

    Created trending items container and trending-item for the landing page

---
+++ include/landing/trending-item.php
@@ -0,0 +1,29 @@
+<!-- Item 1 -->
+<div class="row p-md-2">
+    <div class="col-md-12 featuredItemContainer">
+                            
+        <!-- Item Image -->
+        <div class="row">
+            <div class="col-md-12 featuredItemImage">
+                <img src="image.jpg" />
+            </div>
+        </div>
+
+        <!-- Item Title -->
+        <div class="row">
+            <div class="col-md-12 pt-md-2">
+                <h4>Product Name</h4>
+            </div>
+        </div>
+
+        <!-- Item Price + Action Items -->
+        <!-- Add to cart, price -->
+        <div class="row">
+            <div class="col-md-12 pt-md-2">
+                <h6 class="float-left">$20.00</h6>
+                <h6 class="float-right">Add to cart</h6>
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ include/landing/trendingCon.php
@@ -0,0 +1,75 @@
+<div class="row justify-content-center pt-md-2 pb-md-4">
+    <div class="col-md-11">
+
+        <h4>Trending Items</h4>
+
+        <!-- Top Row -->
+        <div class="row">
+            
+            <!-- Column 1 -->
+            <div class="col-md-3">
+            <?php
+                require('trending-item.php');
+            ?>
+            </div>
+            
+            <!-- Column 2 -->
+            <div class="col-md-3">
+            <?php
+                require('trending-item.php');
+            ?>
+            </div>
+            
+            <!-- Column 3 -->
+            <div class="col-md-3">
+            <?php
+                require('trending-item.php');
+            ?>
+            </div>
+            
+            <!-- Column 4 -->
+            <div class="col-md-3">
+            <?php
+                require('trending-item.php');
+            ?>
+            </div>
+
+        </div>
+        <!-- END OF TOP ROW -->
+
+        <!-- Bottom Row -->
+        <div class="row">
+            
+            <!-- Column 1 -->
+            <div class="col-md-3">
+            <?php
+                require('trending-item.php');
+            ?>
+            </div>
+            
+            <!-- Column 2 -->
+            <div class="col-md-3">
+            <?php
+                require('trending-item.php');
+            ?>
+            </div>
+            
+            <!-- Column 3 -->
+            <div class="col-md-3">
+            <?php
+                require('trending-item.php');
+            ?>
+            </div>
+            
+            <!-- Column 4 -->
+            <div class="col-md-3">
+            <?php
+                require('trending-item.php');
+            ?>
+            </div>
+
+        </div>
+        <!-- END OF BOTTOM ROW -->
+
+    </div>
+</div>
