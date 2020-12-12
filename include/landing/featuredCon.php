9e29a2b0a349b15a5c8bb34beeaa36da0d7de24a
Ryan Alcorn <ryanalcorn2@gmail.com>
2020-12-12 17:25:29 -0400

    Modified footer sizing, moved featured-items into new folder

---
+++ include/landing/featuredCon.php
@@ -0,0 +1,38 @@
+<div class="col-md-11">
+
+    <h4>Featured Items</h4>
+
+    <!-- -->
+    <div class="row">
+        
+        <!-- Column 1 -->
+        <div class="col-md-3">
+        <?php
+            require('featured-item.php');
+        ?>
+        </div>
+        
+        <!-- Column 2 -->
+        <div class="col-md-3">
+        <?php
+            require('featured-item.php');
+        ?>
+        </div>
+        
+        <!-- Column 3 -->
+        <div class="col-md-3">
+        <?php
+            require('featured-item.php');
+        ?>
+        </div>
+        
+        <!-- Column 4 -->
+        <div class="col-md-3">
+        <?php
+            require('featured-item.php');
+        ?>
+        </div>
+
+    </div>
+
+</div>
