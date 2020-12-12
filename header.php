93ad5585a4c9e9f41f7286667149a0541e2b0087
Ryan Alcorn <ryanalcorn2@gmail.com>
2020-12-12 17:02:34 -0400

    Added comments & spacing

--- header.php
+++ header.php
@@ -1,9 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+  <!-- Toggle -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="#">Navbar</a>
+
+  <!-- LOGO -->
+  <a class="navbar-brand" href="#">BeautyBuy</a>
+
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+
+  <!-- Nav Links -->
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
@@ -15,9 +22,13 @@
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
+
+    <!-- Search Bar & Button -->
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
+
   </div>
+
 </nav>
