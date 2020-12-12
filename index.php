8bf8281556045785a47aa9ba3268b21f393fd2ff
Ryan Alcorn <ryanalcorn2@gmail.com>
2020-12-12 17:52:27 -0400

    Created cardStyle and used it in both featured-item.php and trending-item.php to increase reusability

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 	<link rel="stylesheet" type="text/css" href="style/style.css">
 	<link rel="stylesheet" type="text/css" href="style/header.css">
 	<link rel="stylesheet" type="text/css" href="style/landing.css">
+	<link rel="stylesheet" type="text/css" href="style/cardStyle.css">
 	<link rel="stylesheet" type="text/css" href="style/footer.css">
   </head>
   <body>
