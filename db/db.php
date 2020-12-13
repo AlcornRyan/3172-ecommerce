13988104788efdc4badc6087258cb3f3c5fd2ede
Ryan Alcorn <ryanalcorn2@gmail.com>
2020-12-13 12:54:52 -0400

    Created DB folder and db.php. Working and connects to local DB

---
+++ db/db.php
@@ -0,0 +1,20 @@
+<?php
+
+    /*
+     *  DO NOT MODIFY
+	 *	db.php
+	 *	Connects to the Database on localhost.
+	 */
+
+	$host = "localhost";
+	$un = "root";
+	$pw = "";
+	$db = "beautybuy";
+
+	$conn = new mysqli($host, $un, $pw, $db);
+
+	if ($conn->connect_error) {
+		die ("Error connecting to the DB.<br>" . $conn->connect_error);
+	}
+
+?>
