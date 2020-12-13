a50330f6f4ff83b9e88b36d902a9fdf7998e887b
Ryan Alcorn <ryanalcorn2@gmail.com>
2020-12-12 23:22:02 -0400

    Set up file structure for signin and signup with basic front-end

---
+++ include/signup.php
@@ -0,0 +1,15 @@
+<div class="container-fluid SignUp">
+    <div class="row justify-content-center">
+
+        <!-- Sign up Centered Box -->
+        <div class="col-sm-8 col-md-8">
+        <?php
+
+            // Sign up Container
+            require_once('signup/signupCon.php');
+
+        ?>
+        </div>
+
+    </div>
+</div>
