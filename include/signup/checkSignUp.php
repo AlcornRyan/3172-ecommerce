c5cd8acf68403e3ef82723510b797899a90c1589
Ryan Alcorn <ryanalcorn2@gmail.com>
2020-12-13 20:25:34 -0400

    Fixed bug, sign up working correctly now

--- include/signup/checkSignUp.php
+++ include/signup/checkSignUp.php
@@ -16,7 +16,7 @@
         $myRePassword = validateData($_POST['su_re_pswd']);
 
         /*
-         *  If passwords are not the same, return error
+        *  If passwords are not the same, return error
         */
         if ($myPassword == $myRePassword) { // Passwords match, continue
             
@@ -27,51 +27,59 @@
 
             if (!$conn->query($insertUsersQuery)) {
                 die ("Nooooooooo!" . $conn->error);
-            } else {
+            }
+
+
+            /*
+            * Insert sign up information into login table
+            */
+            $insertLoginQuery = "INSERT into login (`login_email`, `login_pswd`) values('{$myEmail}', '{$myPassword}')";
+
+            if (!$conn->query($insertLoginQuery)) {
+                die ("Nooooooooo!" . $conn->error);
+            }
+
+
+            /*
+            * Get u_id from users and insert it into the login table
+            */
+            $loginIDQuery = "SELECT u_id FROM users WHERE u_email='$myEmail'";
+
+            $result = $conn->query($loginIDQuery);
+
+            $resultArray = array();
+
+            if (mysqli_num_rows($result) != 0) { // if success, go to index.php
+                        
+                /*
+                *  Fetches a single row because email is unique
+                */
+                while ($row = $result->fetch_assoc()) {
+                    array_push($resultArray, $row);
+                }
+            
+                /*
+                *  u_id from users table
+                */
+                $userID = $resultArray[0]['u_id'];
 
 
                 /*
-                 *  Query users table for u_id
+                * Update login_user_id column into login table
                 */
-                $uID_Query = "SELECT u_id FROM users WHERE u_email='$myEmail' AND u_psw='$myPassword'";
+                $updateLoginQuery = "UPDATE login set `login_user_id`='{$userID}' where `login_email`='{$myEmail}'";
 
-                $result = $conn->query($uID_Query);
-
-                $resultArray = array();
-
-                if (mysqli_num_rows($result) != 0) { // if success, go to index.php
-                            
-                    /*
-                    *  Fetches a single row because email is unique
-                    */
-                    while ($row = $result->fetch_assoc()) {
-                        array_push($resultArray, $row);
-                    }
-                
-                    /*
-                    *  login user id of user
-                    */
-                    $userID = $resultArray[0]['u_id'];
-
-
-                    /*
-                     * Insert sign up information into login table
-                    */
-                    $insertLoginQuery = "INSERT into login (`login_user_id`, `login_email`, `login_pswd`) values('{$userID}', '{$myEmail}', '{$myPassword}')";
-
-                    if (!$conn->query($insertUsersQuery)) {
-                        die ("Nooooooooo!" . $conn->error);
-                    } else { // Inserted sign up data successfully into login table
-                        header("location: index.php?page=signin"); // redirect user to sign in page
-                    }
-
+                if (!$conn->query($updateLoginQuery)) {
+                    die ("line 73 error!" . $conn->error);
+                }
+                else { // Successful sign up, redirect user to sign in page!!
+                    header("location: index.php?page=signin&signup=success");
                 }
 
-            }
-            
 
-        }
-        else { // Passwords don't match, return to signup page with error code
+            }
+
+        } else { // Passwords don't match, return to signup page with error code
             header("location: index.php?signup=true&error=pswd_not_matching");
         }
         
