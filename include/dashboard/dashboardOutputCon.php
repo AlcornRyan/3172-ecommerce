f23a2a12788ae2453f46d2b1f48033772c47ec41
Ryan Alcorn <ryanalcorn2@gmail.com>
2020-12-14 16:14:14 -0400

    Dashboard output container header changes based on page user is viewing, backend added and working

--- include/dashboard/dashboardOutputCon.php
+++ include/dashboard/dashboardOutputCon.php
@@ -2,6 +2,56 @@
 <div class="row dash_OutputCon pt-2 pb-4">
     <div class="col-md-12">
 
+        <!-- Dashboard Header -->
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                /*
+                * Dashboard Header Title
+                *
+                * Nav variable NOT SET
+                */
+                if (!isset($_GET['nav'])) {
+                    echo "<h4>Account</h4>";
+                }
+                else {
+                    /*
+                    * Nav variable IS SET
+                    *
+                    * nav == account
+                    */
+                    if ($_GET['nav'] == "account") {
+                        echo "<h4>Account</h4>";
+                    }
+                    /*
+                    * nav == users
+                    */
+                    else if ($_GET['nav'] == "users") {
+                        echo "<h4>Users Dashboard</h4>";
+                    }
+                    /*
+                    * nav == product
+                    */
+                    else if ($_GET['nav'] == "product") {
+                        echo "<h4>Product Dashboard</h4>";
+                    }
+                    /*
+                    * nav == analytics
+                    */
+                    else if ($_GET['nav'] == "analytics") {
+                        echo "<h4>Analytics Dashboard</h4>";
+                    }
+                    /*
+                    * ELSE user is trying to navigate to hidden pages? Just show their account 
+                    */
+                    else {
+                        echo "<h4>Account</h4>";
+                    }
+                }
+                ?>
+            </div>
+        </div>
+
         <!-- Dashboard Item Container -->
         <div class="row">
             
