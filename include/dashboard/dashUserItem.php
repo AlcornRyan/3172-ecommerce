ed4efc25f792f44b70e84957177c65c46af4f340
Ryan Alcorn <ryanalcorn2@gmail.com>
2020-12-14 16:54:48 -0400

    Set up front-end for dashboard users output

---
+++ include/dashboard/dashUserItem.php
@@ -0,0 +1,70 @@
+<!-- Dashboard Item Headings -->
+<div class="row py-2">
+
+    <!-- -->
+    <div class="col-xs-12 col-sm-12 col-md-2 border-right">
+        <b>User ID</b>
+    </div>
+    
+    <!-- -->
+    <div class="col-xs-12 col-sm-12 col-md-3 border-right">
+        <b>Full Name</b>
+    </div>
+    
+    <!-- -->
+    <div class="col-xs-12 col-sm-12 col-md-3 border-right">
+        <b>Email</b>
+    </div>
+    
+    <!-- -->
+    <div class="col-xs-12 col-sm-12 col-md-2 border-right">
+        <b>User Type</b>
+    </div>
+    
+    <!-- -->
+    <div class="col-xs-12 col-sm-12 col-md-2">
+        <b>Store</b>
+    </div>
+
+</div>
+
+<?php
+/*
+* Loop through all users
+*/
+?>
+
+<div class="row dash_ItemEven py-2">
+            
+    <!-- -->
+    <div class="col-xs-12 col-sm-12 col-md-2 border-right">
+        000000111
+    </div>
+    
+    <!-- -->
+    <div class="col-xs-12 col-sm-12 col-md-3 border-right">
+        Full Name
+    </div>
+    
+    <!-- -->
+    <div class="col-xs-12 col-sm-12 col-md-3 border-right">
+        Email
+    </div>
+    
+    <!-- -->
+    <div class="col-xs-12 col-sm-12 col-md-2 border-right">
+        User Type
+    </div>
+    
+    <!-- -->
+    <div class="col-xs-12 col-sm-12 col-md-2">
+        Store
+    </div>
+    <!-- -->
+    <div class="col-md-12 pt-2">
+        <div class="row justify-content-end pr-4">
+            Delete Edit
+        </div>
+    </div>
+
+</div>
--- include/dashboard/dashboardNav.php
+++ include/dashboard/dashboardNav.php
@@ -1,10 +1,10 @@
 <!-- Sort Container -->
-<div class="row dash_NavBar pb-3 mb-3">
+<div class="row dash_NavBar pt-2 pb-3 mb-3">
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="container-fluid">
             
             <!-- -->
-            <div class="row">
+            <div class="row dash_Header">
                 <h4>Dashboard Navigation</h4>
             </div>
 
